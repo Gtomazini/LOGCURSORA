65366cb487eaddd5c77b254799e91a6e342c70eb
Gtomazini <gtomazini@hotmail.com>
2024-03-16 09:20:55 -0300

    +DB

--- Controller.js
+++ Controller.js
@@ -1,5 +1,16 @@
-const express=require=('require');
-const cors=require('cors');
-const bParser=require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const bParser = require('body-parser');
 
 const app=express();
+app.use(cors());
+app.use(bParser.urlencoded({ extended: false}));
+
+app.get('/', (req,res)=>{
+    res.send('teste back');
+});
+
+let port = process.env.PORT || 3000
+app.listen(port, (req,res)=>{
+    console.log('Servidor rodando');
+});

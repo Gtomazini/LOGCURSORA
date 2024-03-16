001d78e431450a8f51c5518454ea7f96bfbbc7b9
Gtomazini <gtomazini@hotmail.com>
2024-03-16 11:20:11 -0300

    teste crud

--- Controller.js
+++ Controller.js
@@ -1,15 +1,56 @@
 const express = require('express');
 const cors = require('cors');
 const bParser = require('body-parser');
+const models=require('./models');
 
 const app=express();
 app.use(cors());
 app.use(bParser.urlencoded({ extended: false}));
+let user=models.User;
+let tracking=models.Tracking;
+let product=models.Product;
 
-app.get('/', (req,res)=>{
-    res.send('teste back');
+//********** */ CRUD BASICO*****************
+
+// CREATE
+app.get('/create', async(req,res)=>{
+    let create =await user.create({
+        name: "joao", 
+        password: "abc",
+        createdAt: new Date(),
+        updateAt: new Date()
+    });
+    res.send('Usuário criado com sucesso!');
 });
 
+// READ
+app.get('/read', async(req,res)=>{
+ let read=await user.findAll({
+    raw:true
+ });
+ console.log(read);
+});
+
+// UPDATE
+app.get('/update', async(req,res)=>{
+    let update=await user.findByPk(1).then((response)=>{
+        response.name='Gabrieis';
+        response.password='novaSenha';
+        response.save();
+    });
+
+   });
+
+// DELETE
+app.get('/delete', async(req,res)=>{
+    user.destroy({
+        where: {
+            id:2
+            }
+        });
+    });
+
+    
 let port = process.env.PORT || 3000
 app.listen(port, (req,res)=>{
     console.log('Servidor rodando');
